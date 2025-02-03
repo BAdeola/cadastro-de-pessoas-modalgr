b1a431b6043dd8f2c4b96eec7230909719fb66f9
BAdeola <brayanadeola@gmail.com>
2025-02-03 18:30:20 -0300

    feat: inicializaing the firsts structures

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TableCandidateComponent } from './pages/table-candidate/table-candidate.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "tabela",
+        component: TableCandidateComponent
+    }
+];
---
+++ src/app/pages/table-candidate/table-candidate.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RegisterLayoutComponent } from '../../components/register-layout/register-layout.component';
+
+@Component({
+  selector: 'app-table-candidate',
+  standalone: true,
+  imports: [
+    RegisterLayoutComponent
+  ],
+  templateUrl: './table-candidate.component.html',
+  styleUrl: './table-candidate.component.scss'
+})
+export class TableCandidateComponent {
+
+}
